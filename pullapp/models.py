534a902ec635911b75fecec34163b4f42cc7dc0b
Léo Poulin <leo@goodbarber.com>
2019-01-31 10:54:27 +0100

    need to split submission model

--- pullapp/models.py
+++ pullapp/models.py
@@ -1,25 +1,8 @@
 import uuid
 
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-
-# Chaque soumission doit être enregistrée en base
-# Une soumission devrait présenter les champs suivant:
-#   id_submission (Integer)
-#   Designation (CharField, 255)  -- Désignation de l'objet
-#   about (TextField) - Description de l'impression, raison ...
-#   Stl (BinaryField / FileField ?) -- Fichier Stl
-#   id_printer (relation) -- Imprimante choisie pour l'impression
-#   Hotend_diameter (Integer / Float ?) -- diamètre de la buse d'impression
-#   Layer_resolution (Integer) -- Hauteur de couche standard
-#   id_material (relation) -- Consommable d'impression
-#   Support (Boolean) -- Nécessite des supports d'impression
-#   Copies (Integer) -- Nombre de même objet à imprimer (0 à 99+)
-#   Submission_date (timestamp)
-#   Update_date (timestamp)
-#   Status (Enum) -- Status du process: Soumis, Accepté, Refusé, En attente, ..
-# La validation des champs suivants doit être fait en pré-publish:
-#   id_material, id_printer
 
 DEFAULT_STATUS = '0'
 STATUS_IMPRESSION = (
@@ -33,50 +16,82 @@ STATUS_IMPRESSION = (
 
 
 class SubmissionModel(models.Model):
+    """
+    Chaque soumission doit être enregistrée en base
+    Une soumission devrait présenter les champs suivant:
+    id_submission (UUIDField)
+    designation (CharField, 255)  -- Désignation de l'objet
+    about (TextField) - Description de l'impression, raison ...
+    stl (BinaryField / FileField ?) -- Fichier Stl
+    id_printer (relation) -- Imprimante choisie pour l'impression
+    Hotend_diameter (Float) -- diamètre de la buse d'impression
+    Layer_resolution (Integer) -- Hauteur de couche standard
+    id_material (relation) -- Consommable d'impression
+    support (Boolean) -- Nécessite des supports d'impression
+    copies (Integer) -- Nombre de même objet à imprimer (0 à 99+)
+    submission_date (timestamp)
+    update_date (timestamp)
+    status (Enum) -- Status du process: Soumis, Accepté, Refusé, En attente, ..
+    """
     id_submission = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    designation = models.CharField(max_length=255)
-    about = models.TextField()
-    stl = models.FileField()
-    # Printer parameters
-    id_printer = models.ForeignKey(
-        PrinterModel, on_delete=models.SET_NULL, blank=False)
-    hotend_diameter = models.FloatField()
-    layer_resolution = models.IntegerField()
-    # material parameters
+    designation = models.CharField(max_length=255, blank=False)
+    about = models.TextField(blank=True)
+    stl = models.FileField(blank=False)
     id_material = models.ForeignKey(
-        MaterialModel, on_delete=models.SET_NULL, blank=False)
-    support = models.BooleanField(default=False)
+        ConsumableModel, on_delete=models.SET_NULL, blank=False)
     copies = models.IntegerField(default=0)
     # misc
     submission_date = models.DateField(auto_now_add=True)
     update_date = models.DateField(auto_now=True)
     status = models.CharField(
         max_length=1, choices=STATUS_IMPRESSION, default=DEFAULT_STATUS)
-
-# Certains paramètres sont en relation avec les paramètres de l'imprimante
-# L'imprimante doit donc avoir une entrée en base, définie selon le modèle:
-#   id_printer (IntegerField)
-#   Name (CharField, 255)
-#   Model (Charfiel, 255)
-#   Layer_precision (Array Field, Integer) -- Précision d'une couche en microns
-#   Hotend_available (Array Field, Integer) -- Diamètre de buses disponibles
-#   X_area_size (Integer) -- Taille X du plateau d'impression en mm
-#   Y_area_size (Integer) -- Taille Y du plateau d'impression en mm
-#   Z_area_size (Integer) -- Taille Z du plateau d'impression en mm
-#   Dual_extruder (Boolean) -- Dispose de deux têtes d'impression
-#   Heated_bed (Boolean) -- Dispose d'un plateau chauffant
+    # Printer parameters: Done by admin
+    id_printer = models.ForeignKey(
+        PrinterModel, on_delete=models.SET_NULL, blank=False)
+    hotend_diameter = models.FloatField()
+    layer_resolution = models.IntegerField()
+    support = models.BooleanField(default=False)
 
 
 class PrinterModel(models.Model):
+    """
+    Certains paramètres sont en relation avec les paramètres de l'imprimante
+    L'imprimante doit donc avoir une entrée en base, définie selon le modèle:
+    id_printer (UUIDField)
+    name (CharField, 255)
+    brand_model (Charfiel, 255)
+    Layer_precision (Array Field, Integer) -- Précision d'une couche en microns
+    Hotend_size (Array Field, Float) -- Diamètre de buses disponibles
+    x_size (Integer) -- Taille X du plateau d'impression en mm
+    y_size (Integer) -- Taille Y du plateau d'impression en mm
+    z_size (Integer) -- Taille Z du plateau d'impression en mm
+    dual_extruder (Boolean) -- Dispose de deux têtes d'impression
+    heated_bed (Boolean) -- Dispose d'un plateau chauffant
+    """
     id_printer = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_lenth=255)
+    brand_model = models.Charfield(max_lenth=255)
+    layer_precision = ArrayField(
+        models.FloatField(), blank=False, default=[0.2])
+    hotend_size = ArrayField(models.FloatField(), blank=False, default=[0.4])
+    x_size = models.IntegerField(blank=False)
+    y_size = models.IntegerField(blank=False)
+    z_size = models.IntegerField(blank=False)
+    dual_extruder = models.BooleanField(default=False)
+    heated_bed = models.BooleanField(default=False)
 
-# Chaque consommable est aussi renseigné en base
-#   id_material (Integer)
-#   type (CharField, 12) -- Type de consommable (PLA, ABS, ...)
-#   variant (Charfield, 255) -- Couleur, Spécial, ...
-#   diameter (Integer / Float) -- Diamètre du filament
-#   available (boolean) -- En stock ou non
 
-
-class MaterialModel(models.Model):
+class ConsumableModel(models.Model):
+    """
+    Chaque consommable est aussi renseigné en base
+    id_material (UUIDField)
+    consumable (CharField, 32) -- Type de consommable (PLA, ABS, ...)
+    variant (Charfield, 255) -- Couleur, Spécial, ...
+    diameter (Float) -- Diamètre du filament
+    available (Boolean) -- En stock ou non
+    """
     id_material = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    consumable = models.CharField(max_length=32, blank=False)
+    variant = models.CharField(max_length=255, blank=False)
+    diameter = models.FloatField(blank=False)
+    available = models.BooleanField(default=True)
