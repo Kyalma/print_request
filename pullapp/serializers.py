77949575bdac35d10fdac7ddecf03120fccf6ced
Léo Poulin <leoraphael.poulin@epitech.eu>
2019-02-07 14:08:27 +0100

    api to test

--- pullapp/serializers.py
+++ pullapp/serializers.py
@@ -1,15 +1,32 @@
 from rest_framework import serializers
 from pullapp.models import SubmissionModel
 from pullapp.models import ConsumableModel
+from pullapp.models import PrinterModel
+from pullapp.models import SlicerParamsModel
 
 
 class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubmissionModel
         fields = ('designation', 'about', 'stl', 'material', 'copies')
+        read_only_fields = ('stl', )
 
 
 class ConsumableSerializer(serializers.ModelSerializer):
     class Meta:
         model = ConsumableModel
         fields = ('consumable', 'variant', 'diameter', 'available')
+        read_only_fields = ('consumable', 'variant', 'diameter')
+
+
+class PrinterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PrinterModel
+        fields = ('name', 'brand_model', 'layer_precision', 'hotend_size',
+                  'x_size', 'y_size', 'z_size', 'dual_extruder', 'heated_bed')
+
+
+class SlicerParamsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SlicerParamsModel
+        fields = ('printer', 'hotend_diameter', 'layer_resolution', 'support')
--- pullapp/views.py
+++ pullapp/views.py
@@ -4,8 +4,12 @@ from rest_framework import mixins, generics
 
 from pullapp.serializers import SubmissionSerializer
 from pullapp.serializers import ConsumableSerializer
+from pullapp.serializers import PrinterSerializer
+from pullapp.serializers import SlicerParamsSerializer
 from pullapp.models import SubmissionModel
 from pullapp.models import ConsumableModel
+from pullapp.models import PrinterModel
+from pullapp.models import SlicerParamsModel
 
 
 class SubmissionListGenericAPI(mixins.ListModelMixin,
@@ -120,3 +124,82 @@ class ConsumableListGenericAPI(mixins.ListModelMixin,
         List all the consumables
         """
         return self.list(request, *args, **kwargs)
+
+
+class PrinterGenericAPI(mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin,
+                        generics.GenericAPIView):
+    serializer_class = PrinterSerializer
+    queryset = PrinterModel.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        """
+        Retreive a printer
+        """
+        return self.retrieve(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        """
+        Update is a printer feature
+        """
+        # TODO
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kargs):
+        """
+        Delete a printer
+        """
+        return self.destroy(request, *args, **kwargs)
+
+
+class PrinterCreateGenericAPI(mixins.CreateModelMixin,
+                              generics.GenericAPIView):
+    serializer_class = PrinterSerializer
+    queryset = PrinterModel.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        """
+        Retreive a printer
+        """
+        return self.create(request, *args, **kwargs)
+
+
+class PrinterListGenericAPI(mixins.ListModelMixin,
+                            generics.GenericAPIView):
+    serializer_class = PrinterSerializer
+    queryset = PrinterModel.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        """
+        List all the printers
+        """
+        return self.list(request, *args, **kwargs)
+
+
+class SlicerParamGenericAPI(mixins.CreateModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.DestroyModelMixin,
+                            generics.GenericAPIView):
+    serializer_class = SlicerParamsSerializer
+    queryset = SlicerParamsModel.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        """
+        Retrieve the current slicing params of a submission
+        """
+        return self.retrieve(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Create slicing params for a submission
+        Only one per submission can exist
+        Return an error if there are existing params
+        """
+        return self.create(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        """
+        Update the slicing params of a submission
+        """
+        return self.partial_update(request, *args, **kwargs)
