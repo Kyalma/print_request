02f23b97c83996f2c5e71447670f22351d2e7b4c
Léo Poulin <leoraphael.poulin@epitech.eu>
2019-02-19 17:06:38 +0100

    working on user signup

---
+++ pullapp/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class UserSignupForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=30, required=False, help_test='Optional')
+    last_name = forms.CharField(
+        max_length=30, required=False, help_test='Optional')
+    email = forms.EmailField(max_length=254, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1',
+                  'password2')
