af113ae20e161b906085fa95eb782c96d5007eaa
Léo Poulin <leoraphael.poulin@epitech.eu>
2019-02-20 17:03:20 +0100

    templates fix

--- pullapp/views/front_views.py
+++ pullapp/views/front_views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+
+from pullapp.forms import UserSignupForm
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserSignupForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -14,5 +16,5 @@ def signup(request):
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+        form = UserSignupForm()
+    return render(request, 'pullapp/signup.html', {'form': form})
